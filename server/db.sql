7c006ed95555588979bb734a20d366829377a32e
Joes-BitGit <josephnalmeida@gmail.com>
2021-04-17 16:55:50 -0700

    Insert SQL Commands

--- server/db.sql
+++ server/db.sql
@@ -10,7 +10,8 @@ CREATE TABLE menu (
   item_id BIGSERIAL NOT NULL PRIMARY KEY,
   item VARCHAR(50) NOT NULL,
   price MONEY NOT NULL,
-  category VARCHAR(50)NOT NULL,
-  size VARCHAR(20)
+  category VARCHAR(50) NOT NULL,
+  size VARCHAR(20) DEFAULT 'Not Applicable'
 );
 
+INSERT INTO menu(item, price, category, size) VALUES ('carne asada', 13.95, 'meat') RETURNING *;
