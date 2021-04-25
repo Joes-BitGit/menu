ecd9319289928c435356c566cb2adb97dafa19b1
Joes-BitGit <josephnalmeida@gmail.com>
2021-04-24 17:56:23 -0700

    Form displays name inputted

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,99 +1,18 @@
 import React, { useState } from "react";
+import SearchBar from "./components/SearchBar.jsx";
 
-function App() {
-  const [name, setName] = useState("");
-  const [price, setPrice] = useState("");
-  const [category, setCategory] = useState("");
-  const [size, setSize] = useState("N/A");
-  const [menu, setMenu] = useState([]);
-  const [display, setDisplay] = useState(false);
+const App = () => {
+  const [item, setItem] = useState("");
 
-  const handleSubmit = (Event) => {
-    setMenu([...menu, { name, price, category, size }]);
-    setName("");
-    setPrice("");
-    setCategory("");
-    setSize("N/A");
-    Event.preventDefault();
-  };
-
-  const handleNameInput = (Event) => {
-    setName(Event.currentTarget.value);
-  };
-
-  const handlePriceInput = (Event) => {
-    setPrice(Event.currentTarget.value);
-  };
-
-  const handleCategoryInput = (Event) => {
-    setCategory(Event.currentTarget.value);
-  };
-
-  const handleSizeInput = (Event) => {
-    setSize(Event.currentTarget.value);
-  };
-
-  const handleDisplayMenu = (Event) => {
-    Event.preventDefault();
-    setDisplay(!display);
+  const handleItemInput = (Event) => {
+    setItem(Event.currentTarget.value);
   };
 
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name:
-          <input
-            type="text"
-            name="name"
-            value={name}
-            onInput={handleNameInput}
-            required
-          />
-        </label>
-        <br />
-        <label>
-          Price:
-          <input
-            type="text"
-            name="price"
-            value={price}
-            onInput={handlePriceInput}
-            required
-          />
-        </label>
-        <br />
-        <label>
-          Category:
-          <input
-            type="text"
-            name="category"
-            value={category}
-            onInput={handleCategoryInput}
-            required
-          />
-        </label>
-        <br />
-        <label>
-          Sizes:
-          <select value={size} onInput={handleSizeInput}>
-            <option disabled value="size">
-              Sizes
-            </option>
-            <option value="small">Small</option>
-            <option value="medium">Medium</option>
-            <option value="large">Large</option>
-            <option defaultValue="null">N/A</option>
-          </select>
-        </label>
-        <br />
-        <button type="submit">Submit</button>
-      </form>
-      <button onClick={handleDisplayMenu}>Display Menu</button>
-      <br />
-      {display ? JSON.stringify(menu, null, 2) : null}
+    <div>
+      <SearchBar item={item} handleItemInput={handleItemInput} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/SearchBar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const SearchBar = (props) => {
+  return (
+    <div>
+      {/* SEARCH BAR */}
+      <form>
+        <input
+          type="text"
+          placeholder="ITEM"
+          value={props.item}
+          onInput={props.handleItemInput}
+        />
+        <p>
+          <select name="category">
+            <option selected disabled value="category">
+              CATEGORY
+            </option>
+            <option value="meat">Meat</option>
+            <option value="marisco">Marisco</option>
+            <option value="drinks">Drinks</option>
+            <option value="dessert">Dessert</option>
+            <option value="appetizers">Appetizers</option>
+            <option value="breakfast">Breakfast</option>
+          </select>
+        </p>
+      </form>
+      <button>SHOW ALL</button>
+      {props.item}
+    </div>
+  );
+};
+
+export default SearchBar;
