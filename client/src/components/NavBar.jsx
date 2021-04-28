d7cdda17df2c2945ec32c4144c0a9625e7d2f95c
Joes-BitGit <josephnalmeida@gmail.com>
2021-04-27 17:12:35 -0700

    Added Navbar Component

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const NavBar = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>La Costa Menu</h1>
+      <ul>
+        <li>
+          <button>Menu</button>
+        </li>
+        <li>
+          <button>Add</button>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default NavBar;
