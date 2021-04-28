0a89882c4126579beff8e8a803b7cf4f32a5b0b0
Joes-BitGit <josephnalmeida@gmail.com>
2021-04-27 19:39:47 -0700

    Switch statement + route component

---
+++ client/src/components/Routes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import AddItem from "../routes/AddItem";
+import UpdateItem from "../routes/UpdateItem";
+import Home from "../routes/Home";
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/add" exact component={AddItem} />
+      <Route path="/update" exact component={UpdateItem} />
+    </Switch>
+  );
+};
+
+export default Routes;
