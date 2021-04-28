4c28ecd172fa14d99666140e9c535d09cc071d62
Joes-BitGit <josephnalmeida@gmail.com>
2021-04-27 19:38:02 -0700

    placeholder routes added

---
+++ client/src/routes/AddItem.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddItem = () => {
+  return <div>ADD ITEM PAGE</div>;
+};
+
+export default AddItem;
---
+++ client/src/routes/UpdateItem.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UpdateItem = () => {
+  return <div>UPDATE ITEM PAGE</div>;
+};
+
+export default UpdateItem;
