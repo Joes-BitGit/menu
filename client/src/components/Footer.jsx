8c0d266ddbf86579f1c5684259abf6dbd9ab0d7b
Joes-BitGit <josephnalmeida@gmail.com>
2021-04-27 17:12:49 -0700

    Added Footer component code

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <address>
+      You may also want to visit us:
+      <br />
+      La Costa Restaurant
+      <br />
+      4119 East Gage Ave
+      <br />
+      Bell, CA 90201
+      <br />
+      USA
+    </address>
+  );
+};
+
+export default Footer;
