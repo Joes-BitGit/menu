afcc894803e9c36e8aa048a757ea0c199beb6442
Joes-BitGit <josephnalmeida@gmail.com>
2021-05-08 18:05:07 -0700

    Add context to table + item prop filters data

--- client/src/components/ItemTable.jsx
+++ client/src/components/ItemTable.jsx
@@ -1,24 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import ItemCard from "./ItemCard.jsx";
-import MenuGenerator from "../apis/MenuGenerator.js";
+import MenuGenerator from "../apis/MenuGenerator";
+import { MenuContext } from "../context/MenuContext.js";
+import useIsMount from "./useIsMount.jsx";
 
-const ItemTable = () => {
-  const [menuItems, setMenuItems] = useState([]);
-  // fetch from backend server
+const ItemTable = (props) => {
+  const { menuItems, setMenuItems } = useContext(MenuContext);
+  const isFirstRender = useIsMount();
   useEffect(() => {
-    const fetchMenuItems = async () => {
-      try {
-        const response = await MenuGenerator.get("/");
-        // console.log(response);
-        setMenuItems(response.data.menu_item);
-      } catch (err) {
-        console.log("ERR, ItemTable.jsx: ", err);
-      }
-    };
-    fetchMenuItems();
-  }, []);
+    if (isFirstRender) {
+      // FETCH FROM DB
+      const fetchMenuItems = async () => {
+        try {
+          const response = await MenuGenerator.get("/");
+          console.log("ITEMTABLE: ", response.data);
+          setMenuItems(response.data.menu_item);
+        } catch (err) {
+          console.log("ERR, useMenuData.jsx: ", err);
+        }
+      };
+      fetchMenuItems();
+    } else {
+      // this works, but has a bug when retyping in search
+      // menuItems arr gets cleared when the item is not found
+      // ruining the data
 
-  // Need to be sable to handle multiple item card coming from ItemCard component
+      // Next up think how to save the context data but still be able to manipulate
+      // data that is on the screen without affect it
+      setMenuItems(
+        menuItems.filter((dish) => {
+          return dish.item.includes(props.item);
+        })
+      );
+    }
+  }, [props.item]);
+
+  // Need to be able to handle multiple item card coming from ItemCard component
   return (
     <div>
       <ol>
