70185d24dad62b5c6f1c8207f6b10b23d5c8e0cd
Joes-BitGit <josephnalmeida@gmail.com>
2021-05-19 13:30:31 -0700

    Deletes an item from db

--- client/src/components/ItemCard.jsx
+++ client/src/components/ItemCard.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useHistory } from "react-router";
+import MenuGenerator from "../apis/MenuGenerator";
+import { MenuContext } from "../context/MenuContext";
 
 const ItemCard = (props) => {
   let history = useHistory();
+  const { menuItems, setMenuItems } = useContext(MenuContext);
+  const handleDeleteItem = async (id) => {
+    try {
+      const del = await MenuGenerator.delete(`/${id}`);
+      // setMenuItems(
+      //   menuItems.filter((menuItem) => {
+      //     return menuItem.item_id !== id;
+      //   })
+      // );
+      history.push(`/`);
+      // console.log(history.location.pathname);
+    } catch (err) {
+      console.log("ERR, handleDeleteItem: ", err);
+    }
+  };
 
   return (
     <>
       <button onClick={() => history.push(`/${props.id}/update`)}>EDIT</button>
-      <button>X</button>
+      <button onClick={() => handleDeleteItem(props.id)}>X</button>
       <div>Name: {props.name}</div>
       <div>Category: {props.category}</div>
       <div className="">SIZE: {props.size} </div>
