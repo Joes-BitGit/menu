2e2994564ef90b7e1483995b9f4a6f0c1c14c566
Joes-BitGit <josephnalmeida@gmail.com>
2021-06-04 16:26:14 -0700

    navbar class' add for css

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,24 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/landing.css";
+import "../styles/navbar.css";
 
 const NavBar = () => {
   return (
-    <div className="navbar-header">
-      <h1>
-        <Link to="/landing">La Costa</Link>
-      </h1>
-      <ul>
-        <li>
-          <Link to="/">SEARCH</Link>
-        </li>
-        <li>
-          <Link to="/add">ADD</Link>
-        </li>
-        <li>
-          <Link to="/landing">LANDING</Link>
-        </li>
-      </ul>
+    <div className="navbar-container">
+      <nav className="navbar">
+        <h1 id="lacosta-logo">
+          <Link to="/landing">La Costa</Link>
+        </h1>
+        <ul className="nav-menu">
+          <li className="nav-links">
+            <Link to="/">SEARCH</Link>
+          </li>
+          <li className="nav-links">
+            <Link to="/add">ADD</Link>
+          </li>
+          <li className="nav-links">
+            <Link to="/landing">LANDING</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
