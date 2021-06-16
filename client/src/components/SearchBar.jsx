55b6103841fa27b8e21042409d28df8713bf9964
Joes-BitGit <josephnalmeida@gmail.com>
2021-06-15 20:05:55 -0700

    Added CSS to the search bar

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -13,19 +13,18 @@ const SearchBar = (props) => {
           value={props.item}
           onInput={props.handleItemInput}
         />
-        <p>
-          <select defaultValue="category" className="select search">
-            <option disabled value="category">
-              CATEGORY
-            </option>
-            <option value="meat">Meat</option>
-            <option value="marisco">Marisco</option>
-            <option value="drinks">Drinks</option>
-            <option value="dessert">Dessert</option>
-            <option value="appetizers">Appetizers</option>
-            <option value="breakfast">Breakfast</option>
-          </select>
-        </p>
+
+        <select defaultValue="category" className="select search">
+          <option disabled value="category">
+            CATEGORY
+          </option>
+          <option value="meat">Meat</option>
+          <option value="marisco">Marisco</option>
+          <option value="drinks">Drinks</option>
+          <option value="dessert">Dessert</option>
+          <option value="appetizers">Appetizers</option>
+          <option value="breakfast">Breakfast</option>
+        </select>
       </form>
     </div>
   );
