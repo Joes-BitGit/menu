105def25d0649fffd944bad7dae64b3140454e9c
Joes-BitGit <josephnalmeida@gmail.com>
2021-06-16 18:30:29 -0700

    html structure for easier landing page management

--- client/src/routes/Landing.jsx
+++ client/src/routes/Landing.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import "../styles/landing.css";
 
 const Landing = () => {
-  return <div>Welcome to La Costa 🏝 🌊</div>;
+  return (
+    <div className="title-wrapper">
+      <h1 className="title">
+        Welcome to La Costa 🏝 🌊
+        <button className="cta-btn">See Menu</button>
+      </h1>
+    </div>
+  );
 };
 
 export default Landing;
