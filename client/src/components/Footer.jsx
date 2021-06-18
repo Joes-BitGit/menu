f798cbd574dfd81f1f8792e8cf2cc990cc6be1cd
Joes-BitGit <josephnalmeida@gmail.com>
2021-06-17 19:38:09 -0700

    Anchor tag to google maps

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,17 +1,19 @@
 import React from "react";
+import "../styles/footer.css";
 
 const Footer = () => {
   return (
     <address className="footer-content">
       You may also want to visit us:
-      <br />
-      La Costa Restaurant
-      <br />
-      4119 East Gage Ave
-      <br />
-      Bell, CA 90201
-      <br />
-      USA
+      <a
+        href="http://maps.google.com/?q=4119+East+Gage+Ave+Bell,+CA+90201"
+        className="location"
+      >
+        <div className="business-name">La Costa Restaurant</div>
+        <div className="business-address-link">
+          4119 East Gage Ave Bell, CA 90201
+        </div>
+      </a>
     </address>
   );
 };
